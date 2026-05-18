8e46c590ea23223f7a458ec813c6534b98cde91a
Veru <vdcm1993@protonmail.com>
2026-05-18 15:10:12 +0200

    control errores

--- Taxímetro.py
+++ Taxímetro.py
@@ -8,49 +8,61 @@ acumulado_marcha = []
 def welcome():
     print(
     """
-    Bienvenide al programa
-    Las tarifas son:
+    Bienvenide a TaxiVeru
+    Las opciones disponibles son:
+    - Empezar: Inicia un trayecto
+    - Finalizar: Termina y ve el precio final
+    - Reiniciar: Vuelve al menú principal y empieza un nuevo trayecto
+    
+    Las tarifas disponibles son:
     - Taxi parado = 2 céntimos por segundo.
     - Taxi en marcha = 5 céntimos por segundo.
     """
     )
 
+def start():
+    print("""
+    ¡Comenzamos!)
+    """
+          )
 
 def calculate():
-    if taxi_parado == True:
+    if taxi_parado:
         start_time_parado = time.time()
         end_time_parado = time.time()
         tiempo_parado = end_time_parado - start_time_parado
-
         acumulado_parado.append(tiempo_parado)
-        return acumulado_parado
 
-    if taxi_parado == False:
+    if not taxi_parado:
         start_time_marcha = time.time()
         end_time_marcha = time.time()
         tiempo_marcha = end_time_marcha - start_time_marcha
         acumulado_marcha.append(tiempo_marcha)
-        return acumulado_marcha
 
 def finish():
     trayecto_finalizado = input("Introduce 'Finalizar' para terminar el trayecto")
     precio_final = (0.002 * sum(acumulado_parado)) + (0.005 * sum(acumulado_marcha))
+
+
     while 'Finalizar' in trayecto_finalizado or 'finalizar' in trayecto_finalizado:
         print(f"""
         ¡Trayecto finalizado! El precio es: {precio_final} €
         """
           )
-        break
-    while not 'Finalizar' in trayecto_finalizado or 'finalizar' in trayecto_finalizado:
-        input("Debes escribir la palabra 'Finalizar'")
-        break
+        return restart()
+
+    else:
+       print("Escribiste mal la palabra, debes escribir la palabra 'Empezar'. Inténtalo de nuevo")
+       start(), calculate(), finish(), restart()
+       return finish()
+
 def restart():
-    nuevo_trayecto = input("Introduzca 'Empezar' para comenzar un nuevo trayecto")
-    while 'Empezar' in nuevo_trayecto or 'empezar' in nuevo_trayecto:
-        welcome()
-        break
-    while not 'Empezar' in nuevo_trayecto or 'empezar' in nuevo_trayecto:
-        input("Debes escribir la palabra 'Empezar'")
+    nuevo_trayecto = input("Introduce 'Reiniciar' para volver al menú principal")
+    while 'Reiniciar' in nuevo_trayecto or 'reiniciar' in nuevo_trayecto:
+        return main()
+    else:
+        print("Escribiste mal la palabra. Debes escribir la palabra 'Reiniciar'. Inténtalo de nuevo")
+        return restart()
 
 
 def main():
@@ -58,16 +70,11 @@ def main():
     inicio = input("Introduce 'Empezar' para iniciar el trayecto")
 
     while 'Empezar' in inicio or 'empezar' in inicio:
-        print("""
-        ¡Comenzamos!)
-        """
-              )
-        calculate()
-        finish()
-        restart()
+        start(), calculate(), finish(), restart()
 
-    while not 'Empezar' in inicio or 'empezar' in inicio:
-        input("Debes escribir la palabra 'Empezar'")
 
+    while not 'empezar' in inicio or not 'Empezar' in inicio:
+        print("Escribiste mal la palabra, debes escribir la palabra 'Empezar'. Inténtalo de nuevo")
+        main()
 
 main()
