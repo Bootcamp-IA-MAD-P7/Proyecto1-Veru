302dc98418545c65aff8b598bace874c976d0750
Veru <vdcm1993@protonmail.com>
2026-05-18 18:21:24 +0200

    opciones 0 y 1

--- Taxímetro.py
+++ Taxímetro.py
@@ -1,64 +1,74 @@
 import time
 import logging
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 taxi_parado = True
 acumulado_parado = []
 acumulado_marcha = []
-
+precio_acumulado_parado = []
+precio_acumulado_marcha= []
 
 def menu():
     print(
-    """
-    ¡Bienvenide a TaxiVeru!
-    Estás en el menú principal.
-    Las opciones disponibles son:
-    - Empezar: Inicia un trayecto
-    - Finalizar: Termina y ve el precio final
-    - Reiniciar: Vuelve al menú principal y empieza un nuevo trayecto
+        """
+        ¡Bienvenide a TaxiVeru!
+        Estás en el menú principal.
+        Las opciones disponibles son:
+        - Empezar: Inicia un trayecto
+        - Finalizar: Termina y ve el precio final
+        - Reiniciar: Vuelve al menú principal y empieza un nuevo trayecto
     
-    Las tarifas disponibles son:
-    - Taxi parado = 2 céntimos por segundo.
-    - Taxi en marcha = 5 céntimos por segundo.
-    """
+        Las tarifas disponibles son:
+        - Taxi parado = 2 céntimos por segundo.
+        - Taxi en marcha = 5 céntimos por segundo.
+        """
     )
+
 
 def start():
     print("""
     ¡Comenzamos!)
     """
           )
+    return modes()
+def modes():
+    modo_taxi = input("Escriba '0' si el taxi está parado, '1' si el taxi está moviéndose o 'Finalizar' para terminar el trayecto")
+    while modo_taxi == '0':
+        return taxi_parado
+
+    while modo_taxi == '1':
+        return not taxi_parado
+    while 'Finalizar' or 'finalizar' in modo_taxi:
+        return finish()
 
 def calculate():
-    if taxi_parado:
+    while taxi_parado:
         start_time_parado = time.time()
         end_time_parado = time.time()
         tiempo_parado = end_time_parado - start_time_parado
-        acumulado_parado.append(tiempo_parado)
-
-    if not taxi_parado:
+        acumulado_parado.append(round(tiempo_parado,ndigits=2))
+        precio_parado = 0.002 * tiempo_parado
+        modes()
+    while not taxi_parado:
         start_time_marcha = time.time()
         end_time_marcha = time.time()
         tiempo_marcha = end_time_marcha - start_time_marcha
-        acumulado_marcha.append(tiempo_marcha)
+        acumulado_marcha.append(round(tiempo_marcha,ndigits=2))
+        precio_marcha = 0.005 * tiempo_marcha
+        precio_acumulado_marcha.append(round(precio_marcha,ndigits=2))
+        modes()
+
 
 def finish():
-    trayecto_finalizado = input("Introduce 'Finalizar' para terminar el trayecto")
     precio_final = (0.002 * sum(acumulado_parado)) + (0.005 * sum(acumulado_marcha))
+    print(f"""
+    ¡Trayecto finalizado! El precio es: {precio_final} €
+    """
+            )
+    return restart()
 
-
-    while 'Finalizar' in trayecto_finalizado or 'finalizar' in trayecto_finalizado:
-        print(f"""
-        ¡Trayecto finalizado! El precio es: {precio_final} €
-        """
-          )
-        return restart()
-
-    else:
-       logging.error("Escribiste mal la palabra, debes escribir la palabra 'Empezar'. Inténtalo de nuevo")
-       start(), calculate(), finish(), restart()
-       return finish()
 
 def restart():
     nuevo_trayecto = input("Introduce 'Reiniciar' para volver al menú principal")
@@ -76,9 +86,9 @@ def main():
     while 'Empezar' in inicio or 'empezar' in inicio:
         start(), calculate(), finish(), restart()
 
-
     while not 'empezar' in inicio or not 'Empezar' in inicio:
         logging.error("Escribiste mal la palabra, debes escribir la palabra 'Empezar'. Inténtalo de nuevo")
         main()
+
 
 main()
